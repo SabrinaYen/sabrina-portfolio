d808afb7dae63b1c2ee9296afec62c7423a06b59
s <s@ss-MacBook-Pro.local>
2026-09-05 10:12:55 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/sabrina-portfolio/' : '/',
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: mode === "production" ? './' : '/',
+    plugins: [react()],
+  }
+});
